f36f25be70a0a10c8767b4ad1a25a069c116d366
dksolo <dbm95mail@gmail.com>
2026-03-06 16:45:46 +0500

    feat: NODE-CRON functionality & arguments. ReadME extended

--- README.md
+++ README.md
@@ -15,12 +15,28 @@ npm install
 cp .env.example .env
 ```
 - Fill in the required vars in .env
-- If you want to save the files to disk, update config variable in app.js
 - Run the app in node 
 
 ```
 node app.js
 ```
+
+### Using arguments 
+
+Arguments are used to overwrite .env variables.
+```
+node app.js --file_format=xlsx // Saves file as output/*.xlsx. Overwrites .env value of FILE_FORMAT
+```
+
+here is a list of arguments:
+- --oauth=some_token - Allows to provide OAuth token. Overwrites .env value of FORMS_OAUTH_TOKEN
+- --surveyID=some_id - Allows to provide some other form ID. Overwrites .env value of SURVEY_ID
+- --upload_files=true - Allows to upload files attached to the form to disk. Can be true or false. Overwrites .env value of UPLOAD_FILES
+- --upload=default - Allows to upload files to disk. Can be default or disk. Overwrites .env value of UPLOAD
+- --file_format=csv - Allows to change the file format of the output. Can be either xlsx or csv. Overwrites .env value of FILE_FORMAT
+- --schedule=once = Allows to change how often the program will make request to the form. Can be once, minutely, hourly, daily, weekly. Overwrites .env value of SCHEDULE.
+
+
 
 ### Using different environment variables
 
--- app.js
+++ app.js
@@ -1,18 +1,73 @@
 import 'dotenv/config';
+import cron from 'node-cron';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import {dirname, join} from 'path';
+import { dirname, join} from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-// All configurations are done in .env file
-const FORMS_PUBLIC_API = process.env.FORMS_PUBLIC_API
-const FORMS_OAUTH_TOKEN = process.env.FORMS_OAUTH_TOKEN
-const SURVEY_ID = process.env.SURVEY_ID    
-const SAVE_LOCALLY = process.env.SAVE_LOCALLY
-const FILE_FORMAT = process.env.FILE_FORMAT
-const UPLOAD = process.env.UPLOAD
-const UPLOAD_FILES = process.env.UPLOAD_FILES
+const args = process.argv.slice(2);
+
+// All configurations are done in .env file but can be overriten by cli args
+const FORMS_PUBLIC_API = process.env.FORMS_PUBLIC_API ? process.env.FORMS_PUBLIC_API : "https://api.forms.yandex.net/v1"
+
+function makeArg(args, flag, alternative, values) {
+    if (args && args.length > 0) {
+        for (const arg of args) {
+            if (arg.includes(`--${flag}=`)) {
+                const value = arg.split('=')[1]
+
+                if (!value) {
+                    console.error(`Empty value for flag ${flag}`)
+                    throw new Error(`Empty value for flag ${flag}`)
+                }
+
+                if (values && values[value]) {
+                    return values[value]
+                } else if (values && !values[value]) {
+                    const keys = Object.keys(values)
+                    // console.error(`Incorrect value for flag ${flag}`)
+                    // console.error(`Possible values are: ${keys}`)
+                    throw new Error(`Incorrect value for flag ${flag}\nPossible values are: ${keys}`)
+                } else {
+                    return value
+                }
+            }
+        }
+    } 
+    return alternative
+}
+
+const configOptions = {
+    "SCHEDULE" : {
+        'once': 'once',
+        'minutely' : '* * * * *',
+        'hourly' : '0 * * * *',
+        'daily' : '0 12 * * *',
+        'weekly' : '0 12 1 * *'
+    },
+    "UPLOAD": {
+        "disk": "disk",
+        "default": "default"
+    },
+    "FILE_FORMAT" : {
+        "xlsx": "xlsx",
+        "csv": "csv",
+    },
+    "UPLOAD_FILES" : {
+        "true": "true",
+        "false": "false"
+    },
+}
+
+const FORMS_OAUTH_TOKEN = makeArg(args, "oauth", process.env.FORMS_OAUTH_TOKEN)
+const SURVEY_ID = makeArg(args, "surveyID", process.env.SURVEY_ID)
+const UPLOAD_FILES = makeArg(args, "upload_files", process.env.UPLOAD_FILES, configOptions.UPLOAD_FILES)
+const UPLOAD = makeArg(args, 'upload', process.env.UPLOAD, configOptions.UPLOAD)
+const FILE_FORMAT = makeArg(args, 'file_format', process.env.FILE_FORMAT, configOptions.FILE_FORMAT)
+const SCHEDULE = makeArg(args, 'schedule', process.env.SCHEDULE, configOptions.SCHEDULE )
+
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -31,8 +86,6 @@ async function checkFinished(taskID) {
         })
         .then(response => response.json())
         .then(data => {
-            // console.log(data)
-            // console.log(data.status)
             if (data.status != 'ok') {
                 return false
             } else {
@@ -115,13 +168,34 @@ async function getForms() {
         });
 }
 
-async function main() {
-    console.log('\nStarting...\n')
-    // console.log('\n\nRunning the process with the following vars...')
-    // console.log(`FORMS_PUBLIC_API: ${FORMS_PUBLIC_API}`)
-    // console.log(`FORMS_OAUTH_TOKEN: ${FORMS_OAUTH_TOKEN}`)
-    // console.log(`SURVEY_ID: ${SURVEY_ID}\n`)
-    // console.log(`SAVE_LOCALLY: ${SAVE_LOCALLY}\n`)
+function getAnswers() {
+    const url = `${FORMS_PUBLIC_API}/surveys/${SURVEY_ID}/answers`
+    console.log(`Checking for upadtes...`)
+
+    return fetch(url, {
+        method: 'GET', // Specify the method
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `OAuth ${FORMS_OAUTH_TOKEN}`
+        }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`${response.status}. Could not process the response`)
+            }
+            
+            return response.json()
+        })
+        .then( data => {
+            // console.log(data)
+            return data.answers
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });        
+}
+
+async function startLoading() {
     const dataID = await getForms()
     if (dataID) {
         while (!await checkFinished(dataID)) {
@@ -129,12 +203,54 @@ async function main() {
             await sleep(7000)
         } 
         console.log('Success. File is ready to be downloaded!')
-        if (SAVE_LOCALLY) {
+        if (UPLOAD == "default") {
             downloadResult(dataID)
             console.log('File is downloaded!')
         }
         console.log(`\nJob's done!`)
     }    
+}
+
+
+function main() {
+    console.log('Running the process with the following settings:')
+    console.log(`FORMS_PUBLIC_API: ${FORMS_PUBLIC_API}`)
+    console.log(`FORMS_OAUTH_TOKEN: ${FORMS_OAUTH_TOKEN}`)
+    console.log(`SURVEY_ID: ${SURVEY_ID}`)
+    // console.log(`SAVE_LOCALLY: ${SAVE_LOCALLY}`)
+    console.log(`FILE_FORMAT: ${FILE_FORMAT}`)
+    console.log(`UPLOAD_FILES: ${UPLOAD_FILES}`)
+    console.log(`UPLOAD: ${UPLOAD}`)
+    console.log(`SCHEDULE: ${SCHEDULE}`)
+    console.log('\n')
+    console.log('Starting in 10 seconds..')
+    sleep(10000)
+
+    let date = new Date()
+    if (SCHEDULE == "once") {
+        console.log('Running once...')
+        startLoading()
+    } else {
+        console.log('Running on a schedule...')
+        console.log(SCHEDULE)
+        cron.schedule(SCHEDULE, async function() {
+            const answers = await getAnswers()
+            if (answers.length > 1) {
+                answers.sort((a, b) => new Date(b.created) - new Date(a.created))
+            }
+            const latestAnswersDate = new Date(answers[0].created)
+            if (latestAnswersDate > new Date(date)) {
+                console.log('Something changed!')
+                console.log(`Latest answer: ${answers[0].created}`)
+                console.log(`Previous date value: ${latestAnswersDate}`)
+                date = new Date(answers[0].created)
+                console.log(`New date value: ${date}`)
+                startLoading()
+            }
+            console.log(`Latest answer: ${latestAnswersDate}`)
+            console.log(`Current date value: ${date}`)
+        });
+    }
 }
 
 main()
